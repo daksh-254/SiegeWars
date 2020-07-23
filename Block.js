e6b33604843455423c5d58fea5efb6e925013b4c
daksh-254 <67149010+daksh-254@users.noreply.github.com>
2020-07-23 16:53:15 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -2,7 +2,7 @@ class Block {
   constructor(x,y,width,height) {
     var options = {
          'density': 1.2,
-         'friction': 1.0,
+         'friction': 0.7,
     }
     this.body = Bodies.rectangle(x,y,width,height,options);
     this.width = width;
