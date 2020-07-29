5a66bc17ddaf4da8b161fddf7a7b11a3c8dd4892
daksh-254 <67149010+daksh-254@users.noreply.github.com>
2020-07-29 14:30:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,8 +11,10 @@ var stand1, stand2;
 
 var slingshot, polygon;
 
+var score = 0;
+
 function setup() {
-  createCanvas(1350,650);
+  createCanvas(1360,655);
 
   engine = Engine.create();
 	world = engine.world;
@@ -108,10 +110,9 @@ function draw() {
    polygon.display();  
    
    drawSprites();
-	
+
    textSize(25);
-   text("Drag the Block to crash the towers", 900, 50);
-   text("Press the space bar to reset the block", 900, 100);
+   text("SCORE:" + score, 1200, 40)
 }
 
 function mouseDragged(){
@@ -122,6 +123,28 @@ function mouseDragged(){
 function mouseReleased(){
 //Flying
   slingshot.fly();
+//scoring
+  block1.score();
+  block2.score();
+  block3.score();
+  block4.score();
+  block5.score();
+  block6.score();
+  block7.score();
+  block8.score();
+  block9.score();
+  block10.score();
+  block11.score();
+  block12.score();
+  block13.score();
+  block14.score();
+  block15.score();
+  block16.score();
+  block17.score();
+  block18.score();
+  block19.score();
+  block20.score();
+  block21.score();  
 }
 
 function keyPressed(){
